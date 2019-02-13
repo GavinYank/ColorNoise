71168f0cdda18f01652c69e010dbf344d0c20fb6
GavinYank <47549199+GavinYank@users.noreply.github.com>
2019-02-13 14:46:54 +0800

    添加Hello World，完善开发环境。

---
+++ Src/Ap/Main.c
@@ -0,0 +1,24 @@
+/*************************************************************************
+* All right reserved.
+* Author: chaofa.yan
+* Date: 2019/02/13
+* History Information
+* Author 		Date 		Description
+* chaofa.yan	2019/02/13 	Created the file
+*************************************************************************/
+
+#include <stdio.h>
+
+int main(int Argc, char* Argv[])
+{
+	Argc = Argc;
+	((void)Argv);
+
+	printf("Hello world\n");
+
+	return (0);
+}
+
+
+
+
